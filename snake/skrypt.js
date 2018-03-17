7c3d117beb7e360bc7b5f512cd49e0a7a76c934e
Kamil <f1g0kam@gmail.com>
2018-03-17 10:43:04 +0100

    snake ongoing

--- snake/skrypt.js
+++ snake/skrypt.js
@@ -1,8 +1,12 @@
+var szer = 0;
+var wys=0;
 function czysc() {
   var canv = document.getElementById('myCanvas');
   var ctx = canv.getContext("2d");
 
   ctx.clearRect(0, 0, canv.width, canv.height);
+  szer = canv.width;
+  wys = canv.height;
 }
 
 function Snake() {
@@ -14,24 +18,37 @@ function Snake() {
   this.vy=1;
 
   this.ruch = function() {
-    this.x+=this.vx;
-    this.y+=this.vy;
+    this.x+=this.vx*this.size;
+    this.y+=this.vy*this.size;
+
+    if(this.x<0) {
+      this.x=0;
+    }
+    if(this.x+this.size > szer) {
+      this.x=szer-this.size;
+    }
+    if(this.y < 0) {
+      this.y=0;
+    }
+    if(this.y +this.size > wys) {
+      this.y=wys-this.size;
+    }
   };
   this.wLewo = function() {
-    this.vx=1;
-    this.xy=0;
+    this.vx=-1;
+    this.vy=0;
   };
   this.wPrawo = function() {
-    this.vx=-1;
-    this.xy=0;
+    this.vx=1;
+    this.vy=0;
   }
   this.wGore = function() {
     this.vx=0;
-    this.xy=-1;
+    this.vy=-1;
   };
   this.wDol = function() {
     this.vx=0;
-    this.xy=1;
+    this.vy=1;
   }
 }
 function rysujWeza(snake) {
@@ -47,7 +64,7 @@ function rysujWeza(snake) {
 
 window.onload = function(){
   var s = new Snake();
-  document.addEventListener('keydown', function(evt) {
+  document.addEventListener('keydown', evt => {
     switch (evt.code) {
       case 'ArrowRight': s.wPrawo(); break;
       case 'ArrowLeft':  s.wLewo();  break;
@@ -57,8 +74,11 @@ window.onload = function(){
   });
   // albo prototyp! document.mojKontext = s;
   // i w srodku handlera uzywac evt.target.mojKontext;
-  setInterval(function () {
+  setInterval(() =>{
+
+    czysc();
     rysujWeza(s);
     s.ruch();
-  }, 1000/50);
+
+  }, 1000/5);
 };
