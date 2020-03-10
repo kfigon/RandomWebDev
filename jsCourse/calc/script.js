472ad37fc1215345e2075bc4ae30106e677def9e
Kamil <kamil.figon@gmail.com>
2020-03-10 21:21:46 +0100

    basic js calc

---
+++ jsCourse/calc/script.js
@@ -0,0 +1,28 @@
+function setResult(value) {
+    const result = document.getElementById('result');
+    result.placeholder = value;
+}
+
+function performOperation(foo) {
+    const first = document.getElementById('first-operand').value;
+    const second = document.getElementById('second-operand').value;
+    if(second && first) {
+        setResult(foo(parseInt(first), parseInt(second)));
+    }
+}
+
+function add() {
+    performOperation((a,b) => a+b);
+}
+
+function minus() {
+    performOperation((a,b) => a-b);
+}
+
+function mult() {
+    performOperation((a,b) => a*b);
+}
+
+function div() {
+    performOperation((a,b) => (a/b).toFixed(2));
+}
