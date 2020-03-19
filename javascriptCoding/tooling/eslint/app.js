43bdc760a85f9b52ca217808cffd2b64c3fe1b7d
Kamil <kamil.figon@gmail.com>
2020-03-19 22:03:03 +0100

    es linter and webpack

---
+++ javascriptCoding/tooling/eslint/app.js
@@ -0,0 +1,7 @@
+// npm install --save-dev eslint
+// go to /node_modules/.bin eslint --init *go to not required when
+// project folder is set correctly in project
+// this will create .eslintrc.json with settings, styles etc.
+// vscode extension is good - create es lint
+
+console.log("hello!");
---
+++ javascriptCoding/tooling/webpack/src/app.js
@@ -0,0 +1,12 @@
+// npm init
+// npm install --save-dev webpack webpack-cli
+// create webpack.config.js <-- not json!
+// add config and add build script in package.json
+
+// will bundle sources, minify scripts
+
+// npm run build
+
+import {foo} from './utils'
+
+console.log(foo());
