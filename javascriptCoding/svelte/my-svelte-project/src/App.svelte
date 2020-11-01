23e046c6b5ecd64f16ffb4a40d8a3254744f39b2
Kamil <kamil.figon@gmail.com>
2020-11-01 18:57:32 +0100

    repo reorg

---
+++ javascriptCoding/svelte/my-svelte-project/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let name;
+	let foo = 'asd';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>asdasd {foo}!</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
