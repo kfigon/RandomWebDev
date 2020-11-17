bbce3cadbe30df2cfdb032645934e38cca357e65
Kamil <kamil.figon@gmail.com>
2020-11-17 19:20:19 +0100

    pending work

--- _projs/_FUNCTIONAL/5. unitConverter/app.js
+++ _projs/_FUNCTIONAL/5. unitConverter/app.js
@@ -1,33 +1,93 @@
 "use strict";
-var Action;
-(function (Action) {
-    Action[Action["FOO"] = 0] = "FOO";
-})(Action || (Action = {}));
-function update(state, action) {
+var Unit;
+(function (Unit) {
+    Unit["Celcius"] = "Celcius";
+    Unit["Fahrenheit"] = "Fahrenheit";
+    Unit["Kelvin"] = "Kelvin";
+})(Unit || (Unit = {}));
+function celcToFahr(val) { return (val * 9 / 5) + 35; }
+function fahrToCelc(val) { return (val - 32) * 5 / 9; }
+function celToKelv(val) { return val + 273.15; }
+function kelvToCel(val) { return val - 273.15; }
+function fahrToKel(val) { return celToKelv(fahrToCelc(val)); }
+function kelToFahr(val) { return celcToFahr(kelvToCel(val)); }
+function update(state) {
+    var _a;
+    const { from, to } = state;
+    const mapper = new Map([
+        [Unit.Celcius, new Map([
+                [Unit.Fahrenheit, celcToFahr],
+                [Unit.Kelvin, celToKelv]
+            ])],
+        [Unit.Fahrenheit, new Map([
+                [Unit.Celcius, fahrToCelc],
+                [Unit.Kelvin, fahrToKel]
+            ])],
+        [Unit.Kelvin, new Map([
+                [Unit.Celcius, kelvToCel],
+                [Unit.Fahrenheit, kelToFahr]
+            ])]
+    ]);
+    const foo = (_a = mapper.get(from)) === null || _a === void 0 ? void 0 : _a.get(to);
+    if (foo && state.sourceValue !== null) {
+        state.targetValue = foo(state.sourceValue);
+    }
     return state;
 }
 function view(dispatch, state) {
-    var div = document.createElement('div');
-    div.innerText = 'hello!';
-    var but = document.createElement('button');
-    but.innerText = 'click me';
-    but.addEventListener('click', function () { return dispatch(Action.FOO); });
-    div.appendChild(but);
+    const div = document.createElement('div');
+    div.appendChild(input(1, (ev) => {
+        console.log('typed');
+        dispatch();
+    }));
+    div.appendChild(combobox([Unit.Celcius, Unit.Fahrenheit, Unit.Kelvin], (ev) => {
+        if (ev && ev.target) {
+            // not an error!
+            console.log(ev.target.value);
+            dispatch();
+        }
+    }));
     return div;
 }
+function combobox(values, inputCallback) {
+    const sel = document.createElement('select');
+    sel.addEventListener('change', inputCallback);
+    const createOption = (val) => {
+        const o = document.createElement('option');
+        o.value = val.toString();
+        o.innerText = val.toString();
+        return o;
+    };
+    values
+        .map(v => createOption(v))
+        .forEach(v => sel.appendChild(v));
+    return sel;
+}
+function input(value, inputCallback) {
+    const inp = document.createElement('input');
+    if (value !== null) {
+        inp.defaultValue = value.toString();
+    }
+    inp.addEventListener('input', inputCallback);
+    return inp;
+}
 function app() {
-    var app = document.getElementById('app');
+    const app = document.getElementById('app');
     if (!app) {
         alert('no app');
         throw new Error("no app");
     }
-    var state = {};
-    var viewNode = view(dispatch, state);
+    let state = {
+        from: Unit.Celcius,
+        to: Unit.Celcius,
+        sourceValue: 0,
+        targetValue: 0,
+    };
+    let viewNode = view(dispatch, state);
     app.appendChild(viewNode);
-    function dispatch(action) {
-        console.log('updating app');
-        state = update(state, action);
-        var updatedView = view(dispatch, state);
+    function dispatch() {
+        state = update(state);
+        const updatedView = view(dispatch, state);
         if (!app)
             return;
         app.replaceChild(updatedView, viewNode);
--- _projs/_FUNCTIONAL/5. unitConverter/app.ts
+++ _projs/_FUNCTIONAL/5. unitConverter/app.ts
@@ -1,45 +1,118 @@
-interface State{
-
+interface State {
+    from: Unit;
+    to: Unit;
+    sourceValue: number | null;
+    targetValue: number | null;
 }
-enum Action{
-    FOO
-}
 
-function update(state: State, action: Action): State {
+enum Unit { Celcius = 'Celcius', Fahrenheit = 'Fahrenheit', Kelvin = 'Kelvin' }
+
+function celcToFahr(val: number): number { return (val * 9 / 5) + 35; }
+function fahrToCelc(val: number): number { return (val - 32) * 5 / 9; }
+
+function celToKelv(val: number): number { return val + 273.15 }
+function kelvToCel(val: number): number { return val - 273.15 }
+
+function fahrToKel(val: number): number { return celToKelv(fahrToCelc(val)); }
+function kelToFahr(val: number): number { return celcToFahr(kelvToCel(val)); }
+
+function update(state: State): State {
+    const { from, to } = state;
+
+    const mapper = new Map<Unit, Map<Unit, Function>>([
+        [Unit.Celcius, new Map<Unit, Function>([
+            [Unit.Fahrenheit, celcToFahr],
+            [Unit.Kelvin, celToKelv]])],
+        [Unit.Fahrenheit, new Map<Unit, Function>([
+            [Unit.Celcius, fahrToCelc],
+            [Unit.Kelvin, fahrToKel]])],
+        [Unit.Kelvin, new Map<Unit, Function>([
+            [Unit.Celcius, kelvToCel],
+            [Unit.Fahrenheit, kelToFahr]])]
+    ]);
+
+    const foo: Function | undefined = mapper.get(from)?.get(to);
+    if (foo && state.sourceValue !== null) {
+        state.targetValue = foo(state.sourceValue);
+    }
+
     return state;
 }
 
-function view(dispatch: Function, state: State) : HTMLElement{
+function view(dispatch: Function, state: State): HTMLElement {
     const div = document.createElement('div');
-    
-    const but = document.createElement('button');
-    but.innerText='click me';
-    but.addEventListener('click', () => dispatch(Action.FOO));
-    
-    div.appendChild(but);
+
+    div.appendChild(input(1,
+        (ev: Event) => {
+            console.log('typed')
+            dispatch();
+        }));
+
+    div.appendChild(combobox([Unit.Celcius, Unit.Fahrenheit, Unit.Kelvin],
+        (ev: Event) => {
+            if (ev && ev.target) {
+                // not an error!
+                console.log(ev.target.value);
+                dispatch();
+            }
+
+        }
+    ));
     return div;
+}
+
+function combobox(values: Unit[], inputCallback: any): HTMLSelectElement {
+    const sel = document.createElement('select');
+    sel.addEventListener('change', inputCallback);
+
+    const createOption = (val: Unit) => {
+        const o = document.createElement('option');
+        o.value = val.toString();
+        o.innerText = val.toString();
+        return o;
+    }
+
+    values
+        .map(v => createOption(v))
+        .forEach(v => sel.appendChild(v));
+
+    return sel;
+}
+
+function input(value: number | null, inputCallback: any): HTMLInputElement {
+    const inp = document.createElement('input');
+    if (value !== null) {
+        inp.defaultValue = value.toString();
+    }
+
+    inp.addEventListener('input', inputCallback);
+    return inp;
 }
 
 function app() {
     const app: HTMLElement | null = document.getElementById('app');
-    if(!app) {
+    if (!app) {
         alert('no app');
         throw new Error("no app");
     }
 
-    let state = {}
+    let state: State = {
+        from: Unit.Celcius,
+        to: Unit.Celcius,
+        sourceValue: 0,
+        targetValue: 0,
+    }
+
     let viewNode = view(dispatch, state);
     app.appendChild(viewNode);
 
-    function dispatch(action: Action) {
-        console.log('updating app');
-        
-       state = update(state, action);
-       const updatedView = view(dispatch, state);
-       if(!app) return;
+    function dispatch() {
+        state = update(state);
+        const updatedView = view(dispatch, state);
+        if (!app) return;
 
-       app.replaceChild(updatedView, viewNode);
-       viewNode = updatedView;
+        app.replaceChild(updatedView, viewNode);
+        viewNode = updatedView;
     }
 }
 
